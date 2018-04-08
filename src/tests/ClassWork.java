40e2077bb5992b20d77545beeb7b3caf5d9f446b
DESKTOP-4CUQ89R\JUBER AHMED <mohammed.juber1993@gmail.com>
2018-04-08 11:56:16 -0400

    MyFirstFramWorkCode

---
+++ src/tests/ClassWork.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ClassWork {
+
+	//Initialize browser
+	@Test
+	public void testingFirstName() {
+
+		WebDriver driver = new ChromeDriver();
+
+		//path of the chrome browser
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\JUBER AHMED\\Framework\\framework2018\\drivers\\chromedriver.exe");	
+		driver.get("https://www.facebook.com/")	;
+	}
+
+}
