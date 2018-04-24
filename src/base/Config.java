38fe75d39ae158bfab0e244911809205cce1012c
DESKTOP-4CUQ89R\JUBER AHMED <mohammed.juber1993@gmail.com>
2018-04-23 23:42:35 -0400

    practice

--- src/base/Config.java
+++ src/base/Config.java
@@ -1,20 +1,22 @@
 package base;
 
 //import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 
 import utils.WebDriverFunctions;
 
 public class Config extends WebDriverFunctions
 {
-	
+
 	@BeforeTest
 	public void initDrivers()
 	{
-	
-    System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
-    driver = new ChromeDriver();
-    driver.get ("http://facebook.com");
+		System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"\\drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		//    System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+		//    driver = new ChromeDriver();
+		driver.get ("http://facebook.com");
 	}
 }
--- src/utils/WebDriverFunctions.java
+++ src/utils/WebDriverFunctions.java
@@ -1,8 +1,8 @@
 package utils;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.log4testng.Logger;
 
 public class WebDriverFunctions {
 	public static Logger APPLICATION_LOGS =Logger.getLogger("devpinoyLogger");
@@ -10,7 +10,6 @@ public class WebDriverFunctions {
 	protected WebDriver driver;
 	
 	public void typeByXpath(String loc, String value) {
-		//Try catch 
 		driver.findElement(By.xpath(loc)).sendKeys(value);
 	}
 
